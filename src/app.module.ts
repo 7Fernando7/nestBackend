bbbdee42a1d14077c3842df70131840c8a7c6f9c
Pereira De Paiva <fernando.pereira@transfesa.com>
2024-01-11 15:47:04 +0100

    Creado modelo y esquema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
+import { User, UserSchema } from './auth/entities/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     
+    MongooseModule.forFeature([
+      { 
+        name: User.name, 
+        schema: UserSchema 
+      }
+    ]),
+
     MongooseModule.forRoot(process.env.MONGO_URI),
 
     AuthModule,
