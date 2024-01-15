d4361a72b441aceb97022c6a878918bb4db7af6c
Pereira De Paiva <fernando.pereira@transfesa.com>
2024-01-15 10:32:38 +0100

    Generar Json Web Token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 import * as bcryptjs from 'bcryptjs';
 
@@ -8,6 +9,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
@@ -15,6 +17,7 @@ export class AuthService {
   constructor(
     @InjectModel( User.name ) 
     private userModel: Model<User>,
+    private jwtService: JwtService,
     ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -64,7 +67,7 @@ export class AuthService {
 
     return {
       user: rest,
-      token: 'ABC-123'
+      token: this.getJwToken({ id: user.id }),
     }
 
     /**
@@ -89,4 +92,13 @@ export class AuthService {
   remove(id: number) {
     return `This action removes a #${id} auth`;
   }
+
+  getJwToken ( payload: JwtPayload ) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
+
+
+
+
 }
